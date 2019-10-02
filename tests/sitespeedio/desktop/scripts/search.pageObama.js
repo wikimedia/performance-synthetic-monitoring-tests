9755fe7bcc7596d16658daa41f36c68669eb0cf9
Peter Hedenskog <phedenskog@wikimedia.org>
2019-10-02 10:07:24 +0200

    Measure internal search.
    Measure searching from the header (direct to Obama) and
    search from the search page.
    
    Bug: T234414
    Change-Id: I357c969578c23ad956a314fff75655bedfbb0a55

---
+++ tests/sitespeedio/desktop/scripts/search.pageObama.js
@@ -0,0 +1,10 @@
+module.exports = async function ( context, commands ) {
+	commands.meta.setTitle( 'Search from the search page' );
+	commands.meta.setDescription( ' Go to the Special:Search page and search for Barack Obama' );
+	await commands.navigate( 'https://en.wikipedia.org/wiki/Special:Search' );
+	await commands.addText.byId( 'Barack Obama', 'ooui-php-1' );
+	await commands.js.run( 'for (let node of document.body.childNodes) { if (node.style) node.style.display = "none";}' );
+	await commands.measure.start( 'searchPageObama' );
+	await commands.click.byClassNameAndWait( 'oo-ui-inputWidget-input oo-ui-buttonElement-button' );
+	return commands.measure.stop();
+};
