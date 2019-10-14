e71f9cbccd44fad43b40cac5bebb5f6b2bb2a62d
Peter Hedenskog <phedenskog@wikimedia.org>
2019-10-14 10:07:08 +0200

    Add extra wait to fix searching in Firefox.
    Bug: T235407
    Change-Id: Id76eff46ad845fba94894cde172adcb15e32ab72

--- tests/sitespeedio/desktop/scripts/search.googleObama.js
+++ tests/sitespeedio/desktop/scripts/search.googleObama.js
@@ -7,5 +7,8 @@ module.exports = async function ( context, commands ) {
 	await commands.js.run( 'for (let node of document.body.childNodes) { if (node.style) node.style.display = "none";}' );
 	await commands.measure.start( 'googleObama' );
 	await commands.click.byXpath( "//a[@href='https://en.wikipedia.org/wiki/Barack_Obama']" );
+	// The extra wait is needed to get the search work in Firefox
+	// https://phabricator.wikimedia.org/T235407
+	await commands.wait.byTime( 2000 );
 	return commands.measure.stop();
 };
