c56498a16d83db3d7c193ee7e52b0d09c071262f
Peter Hedenskog <phedenskog@wikimedia.org>
2019-11-06 10:40:21 +0100

    Test user journey: portal -> main page
    Bug: T234414
    Change-Id: I3f90509a63b3b92407e4aae1f32fc888dcf30cd2

---
+++ tests/sitespeedio/desktop/scripts/secondViewDesktop.mainpage.js
@@ -0,0 +1,9 @@
+module.exports = async function ( context, commands ) {
+	commands.meta.setTitle( 'Test visiting multiple pages' );
+	commands.meta.setDescription( 'First hit the portal page with an empty browser cache and then visit  Main_Page' );
+
+	await commands.navigate( 'https://www.wikipedia.org/' );
+	await commands.wait.byTime( 21000 );
+	await commands.js.run( 'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";' );
+	return commands.measure.start( 'https://en.wikipedia.org/wiki/Main_Page' );
+};
