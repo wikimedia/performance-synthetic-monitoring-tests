2ba6f08527dfd19779bbed7c5912672caa0d0542
Peter Hedenskog <phedenskog@wikimedia.org>
2019-12-08 00:44:25 +0100

    Added missing wait on page to finished loading after click.
    Change-Id: Iada901554b583c0abc583dcaf779a0ff328df3bc

--- tests/sitespeedio/desktop/scripts/search.googleObama.js
+++ tests/sitespeedio/desktop/scripts/search.googleObama.js
@@ -6,7 +6,7 @@ module.exports = async function ( context, commands ) {
 	await commands.click.bySelectorAndWait( 'input[name=btnK]' );
 	await commands.js.run( 'for (let node of document.body.childNodes) { if (node.style) node.style.display = "none";}' );
 	await commands.measure.start( 'googleObama' );
-	await commands.click.byXpath( "//a[@href='https://en.wikipedia.org/wiki/Barack_Obama']" );
+	await commands.click.byXpathAndWait( "//a[@href='https://en.wikipedia.org/wiki/Barack_Obama']" );
 	// The extra wait is needed to get the search work in Firefox
 	// https://phabricator.wikimedia.org/T235407
 	await commands.wait.byTime( 2000 );
