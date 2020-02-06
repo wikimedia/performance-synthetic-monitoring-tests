71da86e427542fc1e9daf86be0e10a9ba155f3cd
Peter Hedenskog <phedenskog@wikimedia.org>
2020-02-06 11:56:02 +0100

    Upgrade to stable 12.0.0 with stable Chrome 80 (instead of beta).
    Change-Id: I3a980782375357c512d331a61d7042f22b050f7e

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_CONTAINER=sitespeedio/sitespeed.io:12.0.0-alpha.3
+DOCKER_CONTAINER=sitespeedio/sitespeed.io:12.0.0
 DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 --name sitespeedio"
 CONFIG="--config /sitespeed.io/config"
 BROWSERS=(chrome firefox)
