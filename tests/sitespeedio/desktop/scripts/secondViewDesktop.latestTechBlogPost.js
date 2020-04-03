a8ad75d236504a1223e83c1e7df1ec6bdee8ec03
Peter Hedenskog <phedenskog@wikimedia.org>
2020-04-03 20:23:00 +0200

    Test the latest published blog post on our Tech blog.
    Run two tests: One hitting the first blog post without any browser cache
    and one with the browser cache filled from the start page of the blog.
    
    Bug: T249322
    Change-Id: Ic30a8ede0afdac16be31205aea0efe0e7321bc26

---
+++ tests/sitespeedio/desktop/scripts/secondViewDesktop.latestTechBlogPost.js
@@ -0,0 +1,17 @@
+module.exports = async function ( context, commands ) {
+
+	commands.meta.setTitle( 'Test visiting the latest Tech blog post.' );
+	commands.meta.setDescription(
+		'First hit the Tech blog start page with an empty browser cache and then visit the latest blog post.'
+	);
+
+	await commands.navigate( 'https://techblog.wikimedia.org/' );
+
+	const elements = await commands.js.run( 'return document.getElementsByClassName("more-link");' ),
+		url = await elements[ 0 ].getAttribute( 'href' );
+	await commands.js.run(
+		'for (let node of document.body.childNodes) { if (node.style) node.style.display = "none";}'
+	);
+
+	return commands.measure.start( url, 'LatestTechBlogPost' );
+};
