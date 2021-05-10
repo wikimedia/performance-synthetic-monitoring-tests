c089e2b51aa61032fb81ebdce3f02ad1cb161ad4
Peter Hedenskog <phedenskog@wikimedia.org>
2021-05-10 14:25:27 +0200

    Increase latency from 100 -> 120 to match CrUx.
    Both desktop and emulated mobile FCP is faster than the CrUx data.
    Increasing the latency will make them match.
    
    Bug: T280621
    Change-Id: Iabefb45a0433eafdebd2b0275728e0d73efc1c0f

--- run.sh
+++ run.sh
@@ -36,7 +36,7 @@ for file in tests/$TEST/*.wpr; do
         POTENTIAL_CONFIG_FILE="config/$TEST/$FILENAME_WITHOUT_EXTENSION.json"
         [[ -f "$POTENTIAL_CONFIG_FILE" ]] && CONFIG_FILE="$POTENTIAL_CONFIG_FILE" || CONFIG_FILE="config/$TEST/$TEST.json"
         [[ -f "$CONFIG_FILE" ]] && echo "Using config file $CONFIG_FILE" for $file || (echo "Missing config file $CONFIG_FILE for $file" && exit 1)
-        docker run $DOCKER_SETUP -e REPLAY=true -e LATENCY=100 $DOCKER_CONTAINER $NAMESPACE --config $CONFIG_FILE -b $browser $file
+        docker run $DOCKER_SETUP -e REPLAY=true -e LATENCY=120 $DOCKER_CONTAINER $NAMESPACE --config $CONFIG_FILE -b $browser $file
         control
     done
 done
