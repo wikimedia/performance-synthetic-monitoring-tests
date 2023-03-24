58b230232688a9b1894a99337ee87bfbe7b5e1ef
Peter Hedenskog <phedenskog@wikimedia.org>
2023-03-24 07:45:05 +0100

    Fix broken search.
    I've updated to run a beta version of sitespeed.io that default to
    ESM modules, but I missed fixing the include to search. This fixes
    that.
    
    Bug: T332969
    Change-Id: Iea3cd54b0b15caf2a3e4cbe30fdaa39aa86c8a23

--- tests/desktop/searchLegacyObama.cjs
+++ tests/desktop/searchLegacyObama.cjs
@@ -1,5 +1,5 @@
 const
-	factory = require( './shared/searchScriptFactory' ),
+	factory = require( './shared/searchScriptFactory.cjs' ),
 	URL = 'https://fr.wikipedia.org/wiki/Barack_Obama?useskin=vector';
 
 module.exports = factory( 'mwVectorLegacySearch', URL, 'highlight' );
