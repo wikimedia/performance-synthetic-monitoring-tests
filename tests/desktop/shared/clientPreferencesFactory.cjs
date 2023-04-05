a7eccaab6e432c8e6a8091398cf1c6eda5031d31
Nicholas Ray <nray@wikimedia.org>
2023-04-04 21:35:14 -0600

    Add desktop client preference user journey tests
    Add two tests, "clientPreferencesBefore" and "clientPreferencesAfterCookie". The
    former measures the performance of a static page in production without client
    preferences, while the latter measures the performance of the static page with
    20 disabled client preferences.
    
    A similar setup was done for mobile at
    I2f2bc43d9f4ae98e4642ac70d49d4a5e2d137751. This one measure the performance
    impact on the desktop site.
    
    Depends-On: I0181a404ec1c353e88d4ef543ea56fda1eb355f4
    Bug: T331681
    Change-Id: I53f00d74cbc88bb4806386a7e2142bf19f5a79f5

---
+++ tests/desktop/shared/clientPreferencesFactory.cjs
@@ -0,0 +1,15 @@
+const URL_BASE = 'https://en.wikipedia.org';
+
+module.exports = function ( title, description, path, onMainPage ) {
+	return async function ( context, commands ) {
+		commands.meta.setTitle( title );
+		commands.meta.setDescription( description );
+
+		// Visit Main_Page and execute onMainPage callback.
+		await commands.navigate( `${URL_BASE}` );
+		await onMainPage(commands);
+		await commands.js.run( 'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";' );
+
+		return commands.measure.start( `${URL_BASE}${path}`);
+	}
+}
