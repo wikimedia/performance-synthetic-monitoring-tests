25a23436ab0a22ebd905ff595509286ae2a10a15
Moh'd Khier Abualruz <mkh.ruzz@live.com>
2023-07-18 19:07:32 +0300

    Run a synthetic test for client side preferences
    - Setting up variables for tests
    - Measure the difference in performance between reading cookies and using local storage with json parse
    
    Bug: T336527
    Bug: T339268
    Change-Id: Iaad22754a9cc44df2f725b4ce80e4277df511769

---
+++ tests/desktop/localStorageClientPreferencesAfterLocalStorageJsonParse.cjs
@@ -0,0 +1,13 @@
+const factory = require( './shared/clientPreferencesFactory.cjs' );
+
+module.exports = factory(
+	'Test After single local storage key having client preferences within JSON string value',
+	'Use a single local storage key to store multiple client preferences within a JSON string value',
+	"/speed-tests/Brazil.enwiki.1164571109/after-local-storage-json-parse/index.html",
+	async () => {
+		await commands.js.run( `
+		    // Setup local storage.
+			localStorage.setItem( 'mw-client-preferences', '{ "vector-feature-limited-width": 1 }' );
+		` );
+	}
+);
---
+++ tests/desktop/localStorageClientPreferencesBefore.cjs
@@ -0,0 +1,13 @@
+const factory = require( './shared/clientPreferencesFactory.cjs' );
+
+module.exports = factory(
+	'Test before multiple local storage client preferences',
+	'Baseline test for local storage client preferences',
+	"/speed-tests/Brazil.enwiki.1164571109/before/index.html",
+	async () => {
+		await commands.js.run( `
+		    // Setup and cookie.
+			document.cookie = 'enwikimwclientprefs=vector-feature-limited-width; path=/; domain=.wikipedia.org; secure; samesite=lax';
+		` );
+	}
+);
