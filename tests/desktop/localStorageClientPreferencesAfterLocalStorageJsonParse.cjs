7a3cc487b534e312c79d4ef84088d246363fcc37
Peter Hedenskog <phedenskog@wikimedia.org>
2023-07-30 13:08:07 +0200

    Add missing commands pass on.
    Change-Id: I3674915924654771b93930f2ed1e959fdcf2fe64

--- tests/desktop/localStorageClientPreferencesAfterLocalStorageJsonParse.cjs
+++ tests/desktop/localStorageClientPreferencesAfterLocalStorageJsonParse.cjs
@@ -3,8 +3,8 @@ const factory = require( './shared/clientPreferencesFactory.cjs' );
 module.exports = factory(
 	'Test After single local storage key having client preferences within JSON string value',
 	'Use a single local storage key to store multiple client preferences within a JSON string value',
-	"/speed-tests/Brazil.enwiki.1164571109/after-local-storage-json-parse/index.html",
-	async () => {
+	'/speed-tests/Brazil.enwiki.1164571109/after-local-storage-json-parse/index.html',
+	async (commands) => {
 		await commands.js.run( `
 		    // Setup local storage.
 			localStorage.setItem( 'mw-client-preferences', '{ "vector-feature-limited-width": 1 }' );
