b463737ff936cb9ad779b73bbd25e40919127022
Phedenskog <phedenskog@wikimedia.org>
2023-08-14 13:51:28 +0000

    Revert "Update Docker container with Firefox 116."
    This reverts commit ab81c1debdd06372aaeb69ed121790bd999fd91b.
    
    Reason for revert: There's a lot of errors coming through but for Chrome today. There should not be any Chrome changes, but to be sure lets revert.
    
    Change-Id: I4b1e18f89c3235511f76002b590006eb7b4fa470

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=29.2.0
+VERSION=29.0.0
 DOCKER_CONTAINER=sitespeedio/sitespeed.io:$VERSION
 DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 --name sitespeedio"
 
