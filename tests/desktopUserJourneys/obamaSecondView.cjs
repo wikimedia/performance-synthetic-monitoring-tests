804a55e6b76f2508f9a15169d95cf3e125f6b844
Peter Hedenskog <phedenskog@wikimedia.org>
2023-09-12 15:02:34 +0200

    Increase wait time between runs and between going to next page.
    Lets try this for the Obama page. Long time ago I remember we hit
    an issue on AWS where a specific wait time made metrics unstable.
    The reason was probably that we hit a time where Chrome phones home
    (even though I tried to turn all that functionality off).
    
    Hopefully changing a couple of different wait times can make the
    instability go away.
    
    Bug: T346118
    Change-Id: Iecc742c0e809684c4855d0469baf617fbf4ea3b3

--- tests/desktopUserJourneys/obamaSecondView.cjs
+++ tests/desktopUserJourneys/obamaSecondView.cjs
@@ -2,7 +2,7 @@ module.exports = async function ( context, commands ) {
 	commands.meta.setTitle( 'Test visiting multiple pages' );
 	commands.meta.setDescription( 'First hit the Main_Page with an empty browser cache and then visit Obama' );
 	await commands.navigate( 'https://en.wikipedia.org/wiki/Main_Page' );
-	await commands.wait.byTime( 21000 );
+	await commands.wait.byTime( 26000 );
 	await commands.js.run( 'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";' );
 	return commands.measure.start( 'https://en.wikipedia.org/wiki/Barack_Obama' );
 };
