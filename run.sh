6e6eeec19a63abb98ccb2740c3ab25cd0313f41c
Peter Hedenskog <phedenskog@wikimedia.org>
2023-09-25 08:20:38 +0200

    Use Docker container with Chrome 117.
    Change-Id: Ic68179ce1d941ee89e3c869380b5db70e77f17a1

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=29.5.0
+VERSION=29.6.0
 DOCKER_CONTAINER=sitespeedio/sitespeed.io:$VERSION
 DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 --name sitespeedio"
 
