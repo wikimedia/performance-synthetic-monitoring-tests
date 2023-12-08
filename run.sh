d4e542f92760a597065fd80a0b4ec68f6419da55
Peter Hedenskog <phedenskog@wikimedia.org>
2023-12-07 16:50:31 -0800

    Update container that uses Cliffs delta.
    Use Cliffs delta to know the size of the significant change.
    
    Bug: T351929
    Change-Id: I3288e6daaed7e98530bceea37ee93f08bcf71316

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=30.7.1
+VERSION=30.8.0
 DOCKER_CONTAINER=sitespeedio/sitespeed.io:$VERSION
 DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 --name sitespeedio"
 DOCKER_SETUP_WPR="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v /baseline/:/baseline -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 --name sitespeedio"
