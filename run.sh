2b3db9d3d642694947f5cfcabbe37fe552b843e7
Peter Hedenskog <phedenskog@wikimedia.org>
2024-01-19 12:04:07 +0100

    Use browser in config file.
    This was broken on the Firefox server that runned both Chrome and Firefox
    tests.
    
    Change-Id: Ia69978f50c9e92f3786b4f70ed2aebe0aeca25e1

--- run.sh
+++ run.sh
@@ -33,33 +33,30 @@ if [[ "$TEST" == *"Replay"* ]]; then
         else
             BROWSERS=(chrome)
         fi
-        for browser in "${BROWSERS[@]}"
+        
+        FILENAME=$(basename -- "$file")
+        FILENAME_WITHOUT_EXTENSION="${FILENAME%.*}"
+        POTENTIAL_CONFIG_FILE="config/$TEST/$FILENAME_WITHOUT_EXTENSION.json"
+        [[ -f "$POTENTIAL_CONFIG_FILE" ]] && CONFIG_FILE="$POTENTIAL_CONFIG_FILE" || CONFIG_FILE="config/$TEST/$TEST.json"
+        [[ -f "$CONFIG_FILE" ]] && echo "Using config file $CONFIG_FILE" for $file || (echo "Missing config file $CONFIG_FILE for $file" && exit 1)
+        # See https://phabricator.wikimedia.org/T282517
+        if [[ $TEST == *"Mobile"* ]]; then
+            LATENCY="-e LATENCY=220"
+        else
+            LATENCY="-e LATENCY=180"
+        fi
+        # Lets test out no latency on one of the servers
+        if [[ $TEST == *"Instant2"* ]]; then
+            LATENCY=""
+        fi
+        if [[ $TEST == *"InstantFirefox"* ]]; then
+            LATENCY=""
+        fi
+        while IFS= read -r url || [ -n "$url" ]
         do
-            FILENAME=$(basename -- "$file")
-            FILENAME_WITHOUT_EXTENSION="${FILENAME%.*}"
-            POTENTIAL_CONFIG_FILE="config/$TEST/$FILENAME_WITHOUT_EXTENSION.json"
-            [[ -f "$POTENTIAL_CONFIG_FILE" ]] && CONFIG_FILE="$POTENTIAL_CONFIG_FILE" || CONFIG_FILE="config/$TEST/$TEST.json"
-            [[ -f "$CONFIG_FILE" ]] && echo "Using config file $CONFIG_FILE" for $file || (echo "Missing config file $CONFIG_FILE for $file" && exit 1)
-            # See https://phabricator.wikimedia.org/T282517
-            if [[ $TEST == *"Mobile"* ]]; then
-                LATENCY="-e LATENCY=220"
-            else
-                LATENCY="-e LATENCY=180"
-            fi
-            # Lets test out no latency on one of the servers
-            if [[ $TEST == *"Instant2"* ]]; then
-                LATENCY=""
-            fi
-            if [[ $TEST == *"InstantFirefox"* ]]; then
-                LATENCY=""
-                BROWSERS=(firefox)
-            fi
-            while IFS= read -r url || [ -n "$url" ]
-            do
-                docker run $DOCKER_SETUP_WPR -e REPLAY=true $LATENCY $DOCKER_CONTAINER $NAMESPACE --config $CONFIG_FILE -b $browser $url
-                control
-            done < "$file"
-        done
+            docker run $DOCKER_SETUP_WPR -e REPLAY=true $LATENCY $DOCKER_CONTAINER $NAMESPACE --config $CONFIG_FILE -b $browser $url
+            control
+        done < "$file" 
     done
 fi
 docker volume prune -f
