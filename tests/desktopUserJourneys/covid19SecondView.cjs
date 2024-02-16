3f2af66aa225d5626a53af1ba69e5a00e8183e14
Peter Hedenskog <phedenskog@wikimedia.org>
2024-02-16 07:55:00 +0100

    Remove covid tests.
    Bug: T357628
    Change-Id: I1f66ceb67e4b496c01df99e7f85f760693a00c83

--- tests/desktopUserJourneys/covid19SecondView.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = async function ( context, commands ) {
-	commands.meta.setTitle( 'Test visiting multiple pages' );
-	commands.meta.setDescription( 'First hit the Main_Page with an empty browser cache and then visit Covid19' );
-	await commands.navigate( 'https://en.wikipedia.org/wiki/Main_Page' );
-	await commands.wait.byTime( 21000 );
-	await commands.js.run( 'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";' );
-	return commands.measure.start( 'https://en.wikipedia.org/wiki/2019–20_coronavirus_pandemic' );
-};
