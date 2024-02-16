f022cf4e6ec5711e16a5aae9b30d38becd22af32
Peter Hedenskog <phedenskog@wikimedia.org>
2024-02-16 09:27:50 +0100

    Rename webpagereplay tests, unify latency and test it.
    There's a couple of changes here, they are done in one go so its easier
    to deploy them (each servers tests needs to be stopped and updated).
    
    We rename the WebPageReplay test folders to make it easier to understand
    what tests runs where. And then we remove setting different latency for
    different tests, we just follow Mozilla and run the tests as fast as we can.
    
    And then as a last thing we change group 1 tests to test on Italian wiki,
    since that content is larger = makes it easier to spot regressions.
    
    Also now run the emulated mobile tests on one server, making it more easy
    to remove them when we gets real devices.
    
    We also start by rebaselining all the tests.
    
    Bug: T356839
    Change-Id: Ib21e05b7b30d156c715fd6fc9909f6ac18b2b1e2

--- run.sh
+++ run.sh
@@ -28,30 +28,12 @@ done
 if [[ "$TEST" == *"Replay"* ]]; then
     for file in tests/$TEST/*.{wpr,cjs}; do
         [ -e "$file" ] || continue
-        if [[ $TEST == *"Mobile"* ]]; then
-            BROWSERS=(chrome)
-        else
-            BROWSERS=(chrome)
-        fi
         EXTRAS=""
         FILENAME=$(basename -- "$file")
         FILENAME_WITHOUT_EXTENSION="${FILENAME%.*}"
         POTENTIAL_CONFIG_FILE="config/$TEST/$FILENAME_WITHOUT_EXTENSION.json"
         [[ -f "$POTENTIAL_CONFIG_FILE" ]] && CONFIG_FILE="$POTENTIAL_CONFIG_FILE" || CONFIG_FILE="config/$TEST/$TEST.json"
         [[ -f "$CONFIG_FILE" ]] && echo "Using config file $CONFIG_FILE" for $file || (echo "Missing config file $CONFIG_FILE for $file" && exit 1)
-        # See https://phabricator.wikimedia.org/T282517
-        if [[ $TEST == *"Mobile"* ]]; then
-            LATENCY="-e LATENCY=220"
-        else
-            LATENCY="-e LATENCY=180"
-        fi
-        # Lets test out no latency on one of the servers
-        if [[ $TEST == *"Instant2"* ]]; then
-            LATENCY=""
-        fi
-        if [[ $TEST == *"InstantFirefox"* ]]; then
-            LATENCY=""
-        fi
         # All WebPageReplay tests use baseline, 
         # and on Sundays we re-baseline!
         DOW=$(date +"%a")
@@ -61,7 +43,7 @@ if [[ "$TEST" == *"Replay"* ]]; then
 
         while IFS= read -r url || [ -n "$url" ]
         do
-            docker run $DOCKER_SETUP_WPR -e REPLAY=true $LATENCY $DOCKER_CONTAINER $NAMESPACE --config $CONFIG_FILE $EXTRAS $url
+            docker run $DOCKER_SETUP_WPR -e REPLAY=true $DOCKER_CONTAINER $NAMESPACE --config $CONFIG_FILE $EXTRAS $url
             control
         done < "$file" 
     done
