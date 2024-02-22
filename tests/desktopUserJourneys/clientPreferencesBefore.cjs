4fca8e6b40129b122ec640b2f27dcc3c5a24e49e
Peter Hedenskog <phedenskog@wikimedia.org>
2024-02-22 16:09:21 +0100

    Remove client preferences cookie test.
    Bug: T357628
    Change-Id: I3e48792d63958a9cd0631be2b4d5a91cfe5388ff

--- tests/desktopUserJourneys/clientPreferencesBefore.cjs
+++
@@ -1,8 +0,0 @@
-const factory = require( './shared/clientPreferencesFactory.cjs' );
-
-module.exports = factory(
-	'Test before multiple client preferences',
-	'Baseline test for client preferences',
-	'/speed-tests/United_States.enwiki.1146952659/before/index.html',
-	async () => {}
-);
