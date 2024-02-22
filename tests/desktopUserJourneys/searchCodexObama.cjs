e1c2fb5dd4e2c336d92c606f6dbe1b1d87b9383f
Peter Hedenskog <phedenskog@wikimedia.org>
2024-02-22 16:25:22 +0100

    Remove codex/legacy search.
    Bug: T357628
    Change-Id: I2dc2463c3b6046320c11e089851118edd0280dbc

--- tests/desktopUserJourneys/searchCodexObama.cjs
+++
@@ -1,5 +0,0 @@
-const
-	factory = require( './shared/searchScriptFactory.cjs' ),
-	URL = 'https://fr.wikipedia.org/wiki/Barack_Obama?useskin=vector-2022';
-
-module.exports = factory( 'mwVectorCodexSearch', URL, 'cdx-search-result-title__match' );
